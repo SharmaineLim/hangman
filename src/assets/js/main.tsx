b62529267459ae5dca0c2f07ccb229982b328170
Sharmaine Lim <amanda.lim@torchbox.com>
2023-06-25 07:47:13 +0800

    Set up Tailwind CSS

--- src/assets/js/main.tsx
+++ src/assets/js/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '../css/index.css';
 
 // Render apps only once DOM content has finished loading.
 document.addEventListener('DOMContentLoaded', () => {
